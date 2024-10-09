ddba7e5861c81b16574599dda964c0ef5f84536b
Szakszi04 <adam.szakszon04@gmail.com>
2024-10-09 17:32:39 +0200

    ezt legyszi olds meg

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -28,6 +28,13 @@ public class TorpedoStore {
     }
   }
 
+
+
+
+
+
+
+  
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
