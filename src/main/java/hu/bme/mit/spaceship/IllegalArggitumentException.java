941c68402808eab3218219ae4eac63ef8707c055
Szakszi04 <adam.szakszon04@gmail.com>
2024-10-09 17:19:31 +0200

    -=javitva

---
+++ src/main/java/hu/bme/mit/spaceship/IllegalArggitumentException.java
@@ -0,0 +1,5 @@
+package hu.bme.mit.spaceship;
+
+public class IllegalArggitumentException {
+
+}
--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -30,7 +30,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArggitumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
